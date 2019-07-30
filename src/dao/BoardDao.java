35bf11216b76a6de973ba6e143106d322c48e686
낙낙 <cnh940214@gmail.com>
2019-07-30 17:30:13 +0900

    Second Commit - 게시판 Insert, Update 기능 구현.

--- src/dao/BoardDao.java
+++ src/dao/BoardDao.java
@@ -75,10 +75,12 @@ public class BoardDao {
 				board.setReadcount(rs.getInt("readcount"));
 				board.setIp(rs.getString("ip"));
 				board.setRef(rs.getInt("ref"));
+				board.setPassword(rs.getString("passwd"));
 			//	board.setRe_level(rs.getInt("re_step"));
 				board.setRe_level(rs.getInt("re_level"));
 				board.setRe_step(rs.getInt("re_step"));
 				board.setReg_date(rs.getDate("reg_date"));
+				board.setContent(rs.getString("content"));
 				list.add(board);
 			}
 		} catch(Exception e) {	System.out.println(e.getMessage()); 
@@ -112,7 +114,7 @@ public class BoardDao {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		Board board = null;
+		Board board = new Board();
 		String sql = "select * from board where num = ?";
 		try {
 			conn = getConnection();
@@ -120,18 +122,22 @@ public class BoardDao {
 			pstmt.setInt(1, num);
 			rs = pstmt.executeQuery();
 			if(rs.next()) {
-				board = new Board();
 				board.setNum(rs.getInt("num"));
 				board.setWriter(rs.getString("writer"));
 				board.setSubject(rs.getString("subject"));
+				board.setContent(rs.getString("content"));
 				board.setEmail(rs.getString("email"));
 				board.setReadcount(rs.getInt("readcount"));
 				board.setIp(rs.getString("ip"));
+				board.setReg_date(rs.getDate("reg_date"));
 				board.setRef(rs.getInt("ref"));
+				board.setPassword(rs.getString("passwd"));
 			//	board.setRe_level(rs.getInt("re_step"));
 				board.setRe_level(rs.getInt("re_level"));
 				board.setRe_step(rs.getInt("re_step"));
-				board.setReg_date(rs.getDate("reg_date"));
+				
+				//board.setContent(rs.getString("content"));
+				
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -142,5 +148,74 @@ public class BoardDao {
 			if (conn !=null) conn.close();
 		}
 		return board;
+	}
+	
+	public int update(Board board) throws SQLException{
+		int result = 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "update board set subject = ?, writer = ?, email = ?, passwd = ?, content = ?, ip = ? where num = ?";
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, board.getSubject());
+			pstmt.setString(2, board.getWriter());
+			pstmt.setString(3, board.getEmail());
+			pstmt.setString(4, board.getPassword());
+			pstmt.setString(5, board.getContent());
+			pstmt.setString(6, board.getIp());
+			pstmt.setInt(7, board.getNum());
+			result = pstmt.executeUpdate();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}finally {
+			if(pstmt != null) pstmt.close();
+			if(conn != null) conn.close();
+		}
+		return result;
+	}
+
+	public int insert(Board board) throws SQLException{
+		int num = board.getNum();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql1 = "select nvl(max(num),0) from board";
+		String sql = "insert into board values(?,?,?,?,?,?,?,?,?,?,?,sysdate)";
+		int result = 0;
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql1);
+			rs = pstmt.executeQuery();
+			rs.next();
+			//key인 num 1씩 증가, mysql auto_increment 또는 oracle sequence
+			//sequence를 사용 : values(시퀀스명(board_seq).nextval,?,?...)
+			int number = rs.getInt(1) + 1;
+			rs.close(); pstmt.close();
+			if(num == 0) board.setRef(number);
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, number);
+			pstmt.setString(2, board.getWriter());
+			pstmt.setString(3, board.getSubject());
+			pstmt.setString(4, board.getContent());
+			pstmt.setString(5, board.getEmail());
+			pstmt.setInt(6, board.getReadcount());
+			pstmt.setString(7, board.getPassword());
+			pstmt.setInt(8, board.getRef());
+			pstmt.setInt(9, board.getRe_step());
+			pstmt.setInt(10, board.getRe_level());
+			pstmt.setString(11, board.getIp());
+			result = pstmt.executeUpdate();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}finally {
+			if(rs != null) rs.close();
+			if(pstmt != null) pstmt.close();
+			if(conn != null) conn.close();
+		}
+		return result;
 	}
 }
---
+++ src/service/UpdateFormAction.java
@@ -0,0 +1,32 @@
+package service;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.Board;
+import dao.BoardDao;
+
+public class UpdateFormAction implements CommandProcess {
+
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		BoardDao bd = BoardDao.getInstance();
+		try {
+			String pageNum = request.getParameter("pageNum");
+			int num = Integer.parseInt(request.getParameter("num"));
+			Board board = bd.select(num);
+			
+			//request.setAttribute("num", num);
+			request.setAttribute("pageNum", pageNum);
+			request.setAttribute("board", board);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+		return "updateForm.jsp";
+	}
+
+}
